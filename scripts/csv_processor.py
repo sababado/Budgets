ba4011658deb9961240f00e310879603142f0397
Robert <robert.james.szabo@gmail.com>
2024-02-14 20:49:16 -0700

    Reformat commits

--- scripts/csv_processor.py
+++ scripts/csv_processor.py
@@ -1,10 +1,11 @@
-# csv prorcessor
+# csv processor
 
 import constant
 import env_setup
 from datetime import datetime
 
 env_setup.init()
+
 
 # get or create output file based on date
 def get_output_file(output_folder_name: str, date: datetime):
@@ -14,16 +15,16 @@ def get_output_file(output_folder_name: str, date: datetime):
     :param date: Date of the file to grab.
     :return: File to work with. Should be closed when done.
     """
-    file_name = output_folder_name + '/' +date.strftime('%d-%m-%Y.csv')
+    file_name = output_folder_name + '/' + date.strftime('%d-%m-%Y.csv')
     return open(file_name, 'a')
-    
+
 
 # get all raw files
-    # determine file type (USAA, Cap1, NFCU)
-    # Process file based on type and append to output file
-    
+# determine file type (USAA, Cap1, NFCU)
+# Process file based on type and append to output file
+
 now = datetime.now()
 with get_output_file(constant.FOLDER_OUTPUT, now) as outFile:
     print("Got file")
-    outFile.write(now.strftime('%d-%m-%Y, %H-%M-%S')+"\n")
+    outFile.write(now.strftime('%d-%m-%Y, %H-%M-%S') + "\n")
     print("done with file")
--- scripts/env_setup.py
+++ scripts/env_setup.py
@@ -4,14 +4,16 @@ import constant
 import os
 from pathlib import Path
 
-def checkOrCreate35Up(folder_name: str):
+
+def check_or_create_35_up(folder_name: str):
     """ Check or create a folder for python version 3.5 and up
     
     :param folder_name: String name of the folder.
     """
-    Path(folder_name).mkdir(parents = True, exists = True)
+    Path(folder_name).mkdir(parents=True, exists=True)
 
-def checkOrCreate35Down(folder_name: str):
+
+def check_or_create_35_down(folder_name: str):
     """ Check or create a folder for python version below 3.5
     
     :param folder_name: String name of the folder.
@@ -20,16 +22,16 @@ def checkOrCreate35Down(folder_name: str):
         os.makedirs(folder_name)
 
 
-def checkOrCreate(folder_name: str):
+def check_or_create(folder_name: str):
     """ Check or create a folder with a name
     
     :param folder_name: String name of the folder.
     """
     # Todo do a version check and run the correct function.
-    checkOrCreate35Down(folder_name)
-  
+    check_or_create_35_down(folder_name)
+
+
 def init():
-    checkOrCreate(constant.FOLDER_RAW)
-    checkOrCreate(constant.FOLDER_OUTPUT)
-    checkOrCreate(constant.FOLDER_LOGS)
-    
+    check_or_create(constant.FOLDER_RAW)
+    check_or_create(constant.FOLDER_OUTPUT)
+    check_or_create(constant.FOLDER_LOGS)
