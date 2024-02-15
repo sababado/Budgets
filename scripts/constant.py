6eb0871a7a092f499f1374b15caedd25df59f875
Robert <robert.james.szabo@gmail.com>
2024-02-14 23:08:17 -0700

    Prepping output file

--- scripts/constant.py
+++ scripts/constant.py
@@ -6,3 +6,5 @@ FOLDER_DATA = "../data"
 FOLDER_RAW = "../data/raw"
 FOLDER_OUTPUT = "../data/output"
 FOLDER_LOGS = "../data/logs"
+
+CSV_OUTPUT_HEADER = "Date,Method,Description,Category,Debit,Credit,Notes,Business or Personal"
--- scripts/csv_processor.py
+++ scripts/csv_processor.py
@@ -1,6 +1,9 @@
 # csv processor
+import os
+from typing import TextIO
 
 import constant
+import csv
 import env_setup
 import logger
 from datetime import datetime
@@ -10,14 +13,20 @@ env_setup.init()
 
 # get or create output file based on date
 def get_output_file(output_folder_name: str, date: datetime):
-    """ Get a file to append new data to.
+    """ Get a file to append new data to. Creates a new file with the name if it doesn't exist.
     
     :param output_folder_name: String name of the folder to use for output.
     :param date: Date of the file to grab.
     :return: File to work with. Should be closed when done.
     """
     file_name = output_folder_name + '/' + date.strftime('%d-%m-%Y.csv')
-    return open(file_name, 'a')
+
+    if not os.path.exists(file_name):
+        csv_file = open(file_name, 'a')
+        csv_file.write(constant.CSV_OUTPUT_HEADER + '\n')
+        return csv_file
+    else:
+        return open(file_name, 'a')
 
 
 # get all raw files
@@ -26,9 +35,10 @@ def get_output_file(output_folder_name: str, date: datetime):
 
 now = datetime.now()
 with logger.Logger(constant.FOLDER_LOGS) as logFile:
+    logFile.print("******* Starting CSV processing *******", "info")
     logFile.print("Log file opened", "info")
     with get_output_file(constant.FOLDER_OUTPUT, now) as outFile:
-        logFile.print("Got file")
+        logFile.print("Got output file", "info")
         outFile.write(now.strftime('%d-%m-%Y, %H-%M-%S') + "\n")
-        logFile.print("done with file")
+        logFile.print("Closing output file", "info")
     logFile.print("--- Closing Log File", "info")
